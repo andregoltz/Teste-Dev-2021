0eb8990b9ce67c44adc47a310a8021f12696bab0
andregoltz <andregoltz@outlook.com.br>
2021-09-15 13:42:42 -0300

    Modifying DataBase

--- Teste.Data/Migrations/20210915164126_Creating Fields.Designer.cs
+++ Teste.Data/Migrations/20210915164126_Creating Fields.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Teste.Data;
 
 namespace Teste.Data.Migrations
 {
     [DbContext(typeof(TesteContext))]
-    partial class TesteContextModelSnapshot : ModelSnapshot
+    [Migration("20210915164126_Creating Fields")]
+    partial class CreatingFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Teste.Data/Migrations/20210915164126_Creating Fields.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Teste.Data.Migrations
+{
+    public partial class CreatingFields : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 15, 13, 41, 26, 458, DateTimeKind.Local).AddTicks(6097),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 9, 13, 17, 33, 55, 166, DateTimeKind.Local).AddTicks(4952));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 15, 13, 41, 26, 457, DateTimeKind.Local).AddTicks(5538),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 9, 13, 17, 33, 55, 165, DateTimeKind.Local).AddTicks(5131));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 13, 17, 33, 55, 166, DateTimeKind.Local).AddTicks(4952),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 9, 15, 13, 41, 26, 458, DateTimeKind.Local).AddTicks(6097));
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateCreated",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2021, 9, 13, 17, 33, 55, 165, DateTimeKind.Local).AddTicks(5131),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2021, 9, 15, 13, 41, 26, 457, DateTimeKind.Local).AddTicks(5538));
+        }
+    }
+}
